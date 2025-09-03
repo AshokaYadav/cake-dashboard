98f19ad2811de0348e9b4346782fe80cf3dd0f16
AshokaYadav <yashoka5111718258@gmail.com>
2025-09-03 17:36:16 +0530

    remove all from layout

--- app/Layoutt.tsx
+++ app/Layoutt.tsx
@@ -1,66 +1,74 @@
-'use client'
-import { useEffect, useState } from 'react';
-import Head from 'next/head';
-import Link from 'next/link';
-import Image from 'next/image';
-import { FiMenu } from 'react-icons/fi';
-import { FaHouse, FaShopify, FaCreditCard, FaCircleUser, FaGift } from "react-icons/fa6"; // or use from `fa` if using older version
-import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from '@/store/store';
-import { useRouter } from 'next/navigation';
-import { logout } from '@/store/features/auth/authSlice';
-import Sidebar from '@/components/Layout/Sidebar';
-import MainContent from '@/components/Layout/MainContent';
-
+"use client";
+import { useEffect, useState } from "react";
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+import { FiMenu } from "react-icons/fi";
+import {
+  FaHouse,
+  FaShopify,
+  FaCreditCard,
+  FaCircleUser,
+  FaGift,
+} from "react-icons/fa6"; // or use from `fa` if using older version
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "@/store/store";
+import { useRouter } from "next/navigation";
+import { logout } from "@/store/features/auth/authSlice";
+import Sidebar from "@/components/Layout/Sidebar";
+import MainContent from "@/components/Layout/MainContent";
 
 const Layout = ({ children }: any) => {
   const [sidebarOpen, setSidebarOpen] = useState(true);
-  const [rightSidebarOpen, setRightSidebarOpen] = useState(false);
-  const [activeTab, setActiveTab] = useState('side1');
-  const [showAddMoneyModal, setShowAddMoneyModal] = useState(false);
+  // const [rightSidebarOpen, setRightSidebarOpen] = useState(false);
+  // const [activeTab, setActiveTab] = useState('side1');
+  // const [showAddMoneyModal, setShowAddMoneyModal] = useState(false);
   const [hoveredSidebar, setHoveredSidebar] = useState(false);
 
   const toggleSidebar = () => setSidebarOpen(!sidebarOpen);
-  const toggleRightSidebar = () => setRightSidebarOpen(!rightSidebarOpen);
-  const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
-  const user = useSelector((state: RootState) => state.auth.user);
-  console.log(user);
-  const router = useRouter();
+  // const toggleRightSidebar = () => setRightSidebarOpen(!rightSidebarOpen);
+  // const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
+  // const user = useSelector((state: RootState) => state.auth.user);
+  // console.log(user);
+  // const router = useRouter();
 
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
-  const handleLogout = () => {
-    dispatch(logout()); // 1. Clear Redux state
-    router.push('/login'); // 2. Redirect to Login page
-  };
+  // const handleLogout = () => {
+  //   dispatch(logout()); // 1. Clear Redux state
+  //   router.push('/login'); // 2. Redirect to Login page
+  // };
 
+  // const [hasCheckedAuth, setHasCheckedAuth] = useState(false);
 
-const [hasCheckedAuth, setHasCheckedAuth] = useState(false);
+  // useEffect(() => {
+  //   // Jaise hi component mount ho, hum bolte hain — auth check start ho gaya
+  //   setHasCheckedAuth(true);
 
-// useEffect(() => {
-//   // Jaise hi component mount ho, hum bolte hain — auth check start ho gaya
-//   setHasCheckedAuth(true);
+  //   if (hasCheckedAuth && !isAuthenticated) {
+  //     router.push('/Login');
+  //   }
+  // }, [isAuthenticated, hasCheckedAuth]);
 
-//   if (hasCheckedAuth && !isAuthenticated) {
-//     router.push('/Login');
-//   }
-// }, [isAuthenticated, hasCheckedAuth]);
-
-  if (!isAuthenticated) {
-    return <div>
-      <main>
-        {children}
-      </main>
-    </div>
-  }
+  // if (!isAuthenticated) {
+  //   return <div>
+  //     <main>
+  //       {children}
+  //     </main>
+  //   </div>
+  // }
 
   return (
-    <div className={`min-h-screen flex flex-col ${sidebarOpen ? 'pl-64' : 'pl-20'}`}>
+    <div
+      className={`min-h-screen flex flex-col ${
+        sidebarOpen ? "pl-64" : "pl-20"
+      }`}
+    >
       <Head>
         <title>Fillerbay Dashboard</title>
       </Head>
 
-{/* //yaha se men inset-0 hataya hai bahi.. */}
+      {/* //yaha se men inset-0 hataya hai bahi.. */}
       {/* Sidebar Overlay */}
       {sidebarOpen && (
         <div
@@ -77,13 +85,8 @@ const [hasCheckedAuth, setHasCheckedAuth] = useState(false);
         setHoveredSidebar={setHoveredSidebar}
       />
 
-
       {/* Main Content */}
-      <MainContent toggleSidebar={toggleSidebar}>
-        {children}
-      </MainContent>
-
-  
+      <MainContent toggleSidebar={toggleSidebar}>{children}</MainContent>
     </div>
   );
 };
